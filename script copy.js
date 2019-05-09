5cd460d8492bd3b83c3ad00d1588f4dff766d166
Iga <iga.naskret@gmail.com>
2019-05-09 18:44:03 +0200

    last changes

--- script copy.js
+++ script copy.js
@@ -3,47 +3,37 @@ let myLink = "https://andreamakarova.dk/kea/wp-huset/wp-json/wp/v2/events?_embed
 const template = document.querySelector("template").content;
 const parent = document.querySelector("main");
 
-
-
-
-
-
-function loadData(){
-fetch(myLink).then(e => e.json()).then(show);
+function loadData() {
+    fetch(myLink).then(e => e.json()).then(show);
 }
 
-function show(data){
-data.forEach(post => {
-   
-     
+function show(data) {
+    data.forEach(post => {
+        //clone
 
-    //clone
-    
-    const clone = template.cloneNode(true);
-    //populate
-    const h1 = clone.querySelector("h1");
-    const h2 = clone.querySelector("h2");
-    const img = clone.querySelector("img");
-    const location = clone.querySelector(".location");
-    const price = clone.querySelector(".price");
-    const article = clone.querySelector("article"); 
-    
-article.classList.add(post.event_category);
+        const clone = template.cloneNode(true);
+        //populate
+        const h1 = clone.querySelector("h1");
+        const h2 = clone.querySelector("h2");
+        const img = clone.querySelector("img");
+        const location = clone.querySelector(".location");
+        const price = clone.querySelector(".price");
+        const article = clone.querySelector("article");
 
-    
-    h1.textContent = post.title.rendered;
-    h2.innerHTML =  post.event_date;
-    location.innerHTML = post.location;
-    price.innerHTML = post.ticket_price + " dkk";
-    img.src = post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
-    
-    clone.querySelector("a").href = "sub-page.html?id=" + post.id;
-    console.log(post);
+        article.classList.add(post.event_category);
 
-    //append
-    parent.appendChild(clone);
-}
-);
+        h1.textContent = post.title.rendered;
+        h2.innerHTML = post.event_date;
+        location.innerHTML = post.location;
+        price.innerHTML = post.ticket_price + " dkk";
+        img.src = post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+
+        clone.querySelector("a").href = "sub-page.html?id=" + post.id;
+        console.log(post);
+
+        //append
+        parent.appendChild(clone);
+    });
 }
 
 loadData(myLink);
